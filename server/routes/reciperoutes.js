8b60536e2cc60b9f2472ebcade9ef56e104d6477
Siddharth Roy <roysiddharth2301@gmail.com>
2022-02-04 23:15:44 +0530

    commit

--- server/routes/reciperoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const recipecontroller = require('../controllers/recipecontroller');
-
-router.get('/', recipecontroller.homepage);
-module.export = router;
