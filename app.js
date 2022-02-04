58d54f822256842fdd2eaaf5a54ce773b3a7a979
TheFounder23 <89129627+TheFounder23@users.noreply.github.com>
2022-02-04 23:51:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const express= require('express')
+const express= require('express');
+const { connect } = require('mongoose');
+// const mongoose = require('mongoose');
+const passport = require('passport');
+const {initializingPassport,isAuthenticated} = require("./passportConfig.js");
+
 const app = express();
 const port = 3000
+const {connectMongoose,User} = require("./database.js");
+const expressSession = require("express-session");
+connectMongoose();
+
+initializingPassport(passport);
+
+app.use(expressSession({secret : "secret",resave :false,
+saveUninitialized:false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended :true}));
 
 app.use(express.static('public'))
 app.use('/css',express.static(__dirname + 'public/css'))
@@ -10,6 +30,7 @@ app.use('/img',express.static(__dirname + 'public/img'))
 
 app.set('views','./views')
 app.set('view engine' , 'ejs')
+
 
 app.get('',(req,res) =>{ //doubt how is the index the root file and for the bout we have to do /about
     res.render('index')
@@ -37,6 +58,40 @@ app.get('/stones',(req,res) =>{
     res.render('stones')
     
 })
+ app.get('/IND_EX',(req,res) =>{//new
+     res.render('IND_EX')
+ })
+app.get('/login',(req,res) =>{//new
+    res.render('login')
+})
+
+app.get('/register',(req,res) =>{
+    res.render('register')
+    
+})
+
+app.get("/profile",isAuthenticated, (req,res) =>{
+    res.send(req.user);
+})
+
+app.get("/logout" ,(req,res) => {
+    req.logout();
+    res.redirect("/login")
+})
+
+app.post("/login",passport.authenticate("local",{failureRedirect: "/register",
+ successRedirect : "/"}),
+);
+
+app.post("/register",async (req,res) =>{
+    const user = await User.findOne({username: req.body.username});
+
+    if(user) return res.status(400).send("user already exists");
+
+    const newUser = await User.create(req.body);
+     res.status(201).send(newUser);
+});
+
 
 
 app.listen(port,() => console.info(`listening on port ${port}`))
