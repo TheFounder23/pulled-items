319b376231be633921416fb358a0be8018573236
Siddharth Roy <roysiddharth2301@gmail.com>
2022-06-01 10:22:05 +0530

    master

--- database.js
+++ database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 exports.connectMongoose = () =>{
     mongoose
-    .connect('mongodb://localhost:27017/passport')
+    // .connect('mongodb://localhost:27017/passport')
+    .connect('mongodb+srv://sid:siddharth@newcluster.gonjdpx.mongodb.net/test')
     .then((e) =>console.log(`connected to ${e.connection.host}`))
     .catch(e => console.log(e));
 
